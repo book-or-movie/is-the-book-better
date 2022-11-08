215f71e9341ed92cfa92198f635b42835c401c0a
pacoromo <pacoromo@me.com>
2022-11-08 17:56:33 -0500

    images working and dependency array updated

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -11,77 +11,76 @@ const SearchResults = () => {
   const searchQuery = urlParamsValue.title
   const API_KEY_BOOKS = process.env.BOOKS_API_KEY
   const API_KEY_MOVIE = process.env.MOVIE_API_KEY
-  
 
-	useEffect(() => {
-		axios({
-			url: `https://api.themoviedb.org/3/search/movie/`,
-			params: {
-				api_key: "372d3f4f5198c56ab56f69a5848e02d3",
+  useEffect(() => {
+    axios({
+      url: `https://api.themoviedb.org/3/search/movie/`,
+      params: {
+        api_key: "372d3f4f5198c56ab56f69a5848e02d3",
         query: searchQuery,
-        
-			},
-		})
-			.then((response) => {
-				const newMovieState = response.data.results.filter((movie) => {
-					return movie.title.toLowerCase() === searchQuery.toLowerCase()
-				})
-				console.log(newMovieState)
-				setMovieData(newMovieState)
-			})
-			.catch((error) => {
-				console.log("error!")
-			})
-	},[])
 
-	useEffect(() => {
-		axios({
-			url: "https://www.googleapis.com/books/v1/volumes/",
-			params: {
-				q: searchQuery,
-				key: API_KEY_BOOKS,
-				language: "en",
-			},
-		}).then((response) => {
-			const newBookState = response.data.items.filter((book) => {
-				return book.volumeInfo.title.toLowerCase() === searchQuery.toLowerCase()
-			})
-			console.log(newBookState)
+      },
+    })
+      .then((response) => {
+        const newMovieState = response.data.results.filter((movie) => {
+          return movie.title.toLowerCase() === searchQuery.toLowerCase()
+        })
+        console.log(newMovieState)
+        setMovieData(newMovieState)
+      })
+      .catch((error) => {
+        console.log("error!")
+      })
+  }, [searchQuery])
+
+  useEffect(() => {
+    axios({
+      url: "https://www.googleapis.com/books/v1/volumes/",
+      params: {
+        q: searchQuery,
+        key: API_KEY_BOOKS,
+        language: "en",
+      },
+    }).then((response) => {
+      const newBookState = response.data.items.filter((book) => {
+        return book.volumeInfo.title.toLowerCase() === searchQuery.toLowerCase()
+      })
+      console.log(newBookState)
       setBookData(newBookState)
     }).catch((error => {
       console.log('error!');
     }))
-  }, [])
-  
+  }, [searchQuery])
+
 
   return (
-		<section className="searchResults">
-			<Search />
-			<div className="searchResults">
-				<div className="movieDisplay">
-					<ul className="moviePick">
-						{movieData.map((movieObj) => {
-							return (
-								<li key={movieObj.id}>
-									<img src={`https://api.themoviedb.org/3/search/movie`+ movieObj.backdrop_path} alt={movieObj.title} />
-								</li>
-							)
-						})}
-					</ul>
-				</div>
-				<div className="bookDisplay">
-					<ul className="bookPick">
-						{bookData.map((bookObj) => {
-							return (
-								<li key={bookObj.id}>
-									<img src={bookObj.volumeInfo.imageLinks.thumbnail} alt={bookObj.title} />
-								</li>
-							)
-						})}
-					</ul>
-				</div>
-			</div>
-		</section>
+    <section className="searchResults">
+      <Search />
+      <div className="searchResults">
+        <div className="movieDisplay">
+          <ul className="moviePick">
+            {movieData.map((movieObj) => {
+              return (
+                <li key={movieObj.id}>
+                  <img src={`https://image.tmdb.org/t/p/w200/` + movieObj.poster_path} alt={movieObj.title} />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="bookDisplay">
+          <ul className="bookPick">
+            {bookData.map((bookObj) => {
+              return (
+                <li key={bookObj.id}>
+                  <img src={bookObj.volumeInfo.imageLinks.thumbnail} alt={bookObj.title} />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
