93eebff752d6e68fa3d7239fe1314cb46c980d97
mattvl <mvamlux@gmail.com>
2022-11-11 10:05:51 -0500

    created header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,10 @@
+import logo from'./assets/pexels-rikka-ameboshi-3358707.jpg';
+
 const Header = () => {
     return(
         <header>
             <h1>Is the Book Better?</h1>
+            <img src={logo} className="logo" />
         </header>
 
     )
