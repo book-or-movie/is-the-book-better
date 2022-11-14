b194c961954f2a5ed6505a8c2fdd4a3184c906ff
louistam888 <louiscodes888@gmail.com>
2022-11-14 15:05:46 -0500

    added loading screen and synopsis plus descriptions

--- src/Book.js
+++ src/Book.js
@@ -11,6 +11,7 @@ function Book({ bookArray, setBook, setShowMovie, setShowBook }) {
                 <h2>Please Select a Book</h2>
                 <ul className="book-pick">
                     {bookArray.map((bookObj) => {
+                     
                         return (
                             <li key={bookObj.id}>
                                 <button
@@ -35,6 +36,9 @@ function Book({ bookArray, setBook, setShowMovie, setShowBook }) {
                                 </p>
                                 <p className="book-release">
                                     Release Date: {bookObj.volumeInfo.publishedDate}
+                                </p>
+                                <p className="book-description">
+                                  Summary: {bookObj.volumeInfo.description}
                                 </p>
                             </li>
                         );
