05402d7c053c1d5f27bcf3f4fe21bc0d837bf366
louistam888 <louiscodes888@gmail.com>
2022-11-15 16:10:07 -0500

    did header styling

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,8 @@
-import logo from './assets/pexels-rikka-ameboshi-3358707.jpg';
-
 const Header = () => {
   return (
     <header>
-      <div className="wrapper">
+      <div className="wrapper header-container">
         <h1>Is the book better?</h1>
-        {/* <img src={logo} className="logo" alt='Logo' /> */}
       </div>
     </header>
 
