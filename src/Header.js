f5f622d3fffe78c533ba6bb36a8be9d8cfed720d
louistam888 <louiscodes888@gmail.com>
2022-11-16 18:50:06 -0500

    fixed header height

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <div className="wrapper header-container">
-        <Link to='/'>
-          <h1>Is the book better?</h1>
-        </Link>
+      <div className="wrapper">
+        <div className="header-container">
+          <Link to='/'>
+            <h1>Is the book better?</h1>
+          </Link>
+          <Search />
+        </div>
       </div>
-      <Search />
     </header>
 
   )
