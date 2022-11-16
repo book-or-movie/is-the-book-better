2faceb3ef5633785f29a0c398434979f9df29bf1
SirKittyton <gregcrutchlow@gmail.com>
2022-11-16 16:53:21 -0500

    added styling to comparison and main

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <div className="wrapper header-container">
+      <div className="header-container">
         <Link to='/'>
           <h1>Is the book better?</h1>
         </Link>
