ae59fa487c41b7e8634b6ae941004f60ae1e7a16
SirKittyton <gregcrutchlow@gmail.com>
2022-11-16 15:22:24 -0500

    added styling to comparison and search form as well as fixed links at footer

--- src/Comparison.js
+++ src/Comparison.js
@@ -3,50 +3,52 @@ const Comparison = ({ selectedBook, selectedMovie }) => {
   const bookRating = selectedBook.volumeInfo.averageRating * 2;
   const movieRating = selectedMovie.vote_average;
   return (
-    <section className="comparison">
-
-
-      <div className="wrapper">
-        <div className="decision">
-          {bookRating > movieRating ? (
-            <h3 className="outcome">The Book is better</h3>
-          ) : bookRating < movieRating ? (
-            <h3 className="outcome">The Movie is better</h3>
-          ) : (
-            <h3 className="outcome">It's a tie</h3>
-          )}
-			  </div>
-        
-			  <div className="comparison-container">				  
-				{/* the below code assigns book-won class only if book rating is superior or tie*/}
-				<div className={`book-container ${bookRating >= movieRating ? "book-won" : ""} `}>
-					<div className="book-image-container">
-						<img src={selectedBook.volumeInfo.imageLinks.thumbnail} alt={selectedBook.volumeInfo.title} />
-					</div>
-					<div className="book-info-container">
-						<p>Book Title: {selectedBook.volumeInfo.title}</p>
-						<p>Author(s): {selectedBook.volumeInfo.authors.join(", ")}</p>
-						<p>Average Reader Rating: {bookRating}/10</p>
-						<p>Published Date: {selectedBook.volumeInfo.publishedDate}</p>
-					</div>
+		<section className="comparison">
+			<div className="wrapper">
+				<div className="decision">
+					{bookRating > movieRating ? (
+						<h3 className="outcome">The Book is better</h3>
+					) : bookRating < movieRating ? (
+						<h3 className="outcome">The Movie is better</h3>
+					) : (
+						<h3 className="outcome">It's a tie</h3>
+					)}
 				</div>
 
-				{/* the below code assigns movie-won class only if movie rating is superior pr tie*/}
-				<div className={`movie-container ${bookRating <= movieRating ? "movie-won" : ""}`}>
-					<div className="movie-image-container">
-						<img src={selectedMovie.poster_path} alt={selectedMovie.title} />
+				<div className="comparison-container">
+					{/* the below code assigns book-won class only if book rating is superior or tie*/}
+					<div
+						className={
+							`book-container ${bookRating >= movieRating ? "book-won" : "book-lost"}`}>
+						<div className="book-image-container">
+							<img src={selectedBook.volumeInfo.imageLinks.thumbnail} alt={selectedBook.volumeInfo.title} />
+						</div>
+						<div className="book-info-container">
+							<p>Book Title: {selectedBook.volumeInfo.title}</p>
+							<p>Author(s): {selectedBook.volumeInfo.authors.join(", ")}</p>
+							<p>Average Reader Rating: {bookRating}/10</p>
+							<p>Published Date: {selectedBook.volumeInfo.publishedDate}</p>
+						</div>
 					</div>
-					<div className="movie-info-container">
-						<p>Movie Title: {selectedMovie.title}</p>
-						<p>Released: {selectedMovie.release_date}</p>
-						<p>Average Viewer Rating: {movieRating}/10</p>
+
+					{/* the below code assigns movie-won class only if movie rating is superior pr tie*/}
+					<div
+						className={
+							`movie-container ${bookRating <= movieRating ? "movie-won" : "movie-lost"}`}>
+						<div className="movie-image-container">
+							<img src={selectedMovie.poster_path} alt={selectedMovie.title} />
+						</div>
+						<div className="movie-info-container">
+							<p>Movie Title: {selectedMovie.title}</p>
+							<p>Released: {selectedMovie.release_date}</p>
+							<p>Average Viewer Rating: {movieRating}/10</p>
+						</div>
 					</div>
 				</div>
-			  </div>
 			</div>{" "}
 			{/* wrapper div end */}
 		</section>
-	)
+  )
 };
 
 export default Comparison;
