ddf94f979eb8e9740aef3eea7f73f9baa2374ff8
louistam888 <louiscodes888@gmail.com>
2022-11-15 22:52:56 -0500

    added names to footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
 
     <footer>
       <div className="wrapper">
-        <p>Created at Juno College of Technology  &copy; 2022</p>
+        <p>Created at Juno College of Technology &copy; 2022 by <a href="https://github.com/Pacoromo" target="_blank" rel="noreferrer">Paco Romo,</a> <a href="https://github.com/GregCrutchlow" target="_blank" rel="noreferrer">Greg Crutchlow,</a> <a href="https://github.com/Louistam888" target="_blank" rel="noreferrer">Louis Tam,</a> and <a href="https://github.com/myboyblue519" target="_blank" rel="noreferrer">Matthew Van Luxemborg</a></p>
       </div>
     </footer>
 
